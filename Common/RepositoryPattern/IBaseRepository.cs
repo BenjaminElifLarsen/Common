6c8effd4e35813182fb0523fcc82e06589fd8aeb
Benjamin <beela93@hotmail.com>
2022-09-12 07:44:40 +0200

    base repository return types changed

--- Common/RepositoryPattern/IBaseRepository.cs
+++ Common/RepositoryPattern/IBaseRepository.cs
@@ -5,12 +5,11 @@ using System.Linq.Expressions;
 namespace Common.DataHandling;
 public interface IBaseRepository<TEntity> where TEntity : class, IAggregateRoot
 {
-    public Task Create(TEntity entity);
-    public Task Update(TEntity entity);
-    public Task Delete(TEntity entity);
+    public void Create(TEntity entity);
+    public void Update(TEntity entity);
+    public void Delete(TEntity entity);
     public Task<IEnumerable<TProjection>> AllAsync<TProjection>(BaseQuery<TEntity, TProjection> query) where TProjection : BaseReadModel;
     public Task<IEnumerable<TProjection>> AllByPredicateAsync<TProjection>(Expression<Func<TEntity, bool>> predicate, BaseQuery<TEntity, TProjection> query) where TProjection : BaseReadModel;
     public Task<TProjection> FindByPredicateAsync<TProjection>(Expression<Func<TEntity, bool>> predicate, BaseQuery<TEntity, TProjection> query) where TProjection : BaseReadModel;
-    public Task SaveChanges();
-    //have a save function void Save()
+    public int SaveChanges();
 }
