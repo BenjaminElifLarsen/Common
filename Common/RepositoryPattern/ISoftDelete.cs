5cb0e0b0facbcc0b9041f648f071acbc824cb35e
Benjamin <beela93@hotmail.com>
2022-10-24 13:30:50 +0200

    added soft delete with date

--- Common/RepositoryPattern/ISoftDelete.cs
+++ Common/RepositoryPattern/ISoftDelete.cs
@@ -1,6 +1,12 @@
 ﻿namespace Common.RepositoryPattern;
 public interface ISoftDelete
 {
-    bool Deleted { get; set; }
+    bool Deleted { get; }
     void Delete();
+}
+
+public interface ISoftDeleteDate
+{
+    DateTime? DeletedFrom { get; }
+    void Delete(DateTime? dateTime);
 }
