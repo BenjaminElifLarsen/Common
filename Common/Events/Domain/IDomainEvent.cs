ecc567a4993b1e688b4d7c516f48d0a1266095c3
Benjamin <beela93@hotmail.com>
2022-11-11 11:29:33 +0100

    some domain event work

--- Common/Events/Domain/IDomainEvent.cs
+++ Common/Events/Domain/IDomainEvent.cs
@@ -1,6 +1,7 @@
 ﻿namespace Common.Events.Domain;
-public interface IDomainEvent<TSourceData>
-{ //pratice and testing, not finalised yet and may not be used in final design'
+
+public interface IDomainEvent
+{//pratice and testing, not finalised yet and may not be used in final design'
     //designed out from some reading
     public string AggregateType { get; }
     public int AggregateId { get; }
@@ -10,8 +11,12 @@ public interface IDomainEvent<TSourceData>
     /// When the event was recorded.
     /// </summary>
     public long TimeStampRecorded { get; }
+}
+
+public interface IDomainEvent<TSourceData> : IDomainEvent
+{ 
     /// <summary>
     /// The source data, that is the changes/added/deleted/etc. that was done. 
     /// </summary>
-    public TSourceData Data { get; }
+    public TSourceData Data { get; } //if needed to be stored in a context, could store the data in a json string
 }
--- Common/RepositoryPattern/IAggregateRoot.cs
+++ Common/RepositoryPattern/IAggregateRoot.cs
@@ -1,5 +1,9 @@
-﻿namespace Common.RepositoryPattern;
+﻿using Common.Events.Domain; 
+
+namespace Common.RepositoryPattern;
 public interface IAggregateRoot
 {
-
+    public IEnumerable<IDomainEvent> Evnets { get; }
+    public void AddDomainEvent(IDomainEvent eventItem);
+    public void RemoveDomainEvent(IDomainEvent eventItem);
 }
