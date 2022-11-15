fab946b906e654850ce0d5afa08298b07890db09
Benjamin <beela93@hotmail.com>
2022-11-15 11:08:53 +0100

    added id reference, generic version

---
+++ Common/RepositoryPattern/IdReference.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.RepositoryPattern;
+/// <summary>
+/// Used to prevent accessing one aggregate root via another aggregate.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public record IdReference<T> : ValueObject
+{
+    public T Id { get; private set; }
+
+	public IdReference(T id)
+	{
+		Id = id;
+	}
+}
