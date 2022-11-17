3cbd7e77ee3f070db8089562be154a357ee0943a
Benjamin <beela93@hotmail.com>
2022-11-17 08:52:41 +0100

    updated so the command bus can return Result

--- Common/CQRS/Commands/ICommandBus.cs
+++ Common/CQRS/Commands/ICommandBus.cs
@@ -1,7 +1,9 @@
-﻿namespace Common.CQRS.Commands;
+﻿using Common.ResultPattern;
+
+namespace Common.CQRS.Commands;
 public interface ICommandBus
 { //the registration method need to ensure each command only has a single handle
-    public void RegisterHandler<T>(Action<T> handler) where T : ICommand;
-    public void Publish<T>(T command) where T : ICommand;
-    public void UnregisterHandler<T>(Action<T> handler) where T : ICommand;
+    public void RegisterHandler<T>(Func<T, Result> handler) where T : ICommand; //action does not permit returning data and it could be useful to return a Result
+    public Result Publish<T>(T command) where T : ICommand; //consider switching to Func for the command part
+    public void UnregisterHandler<T>(Func<T, Result> handler) where T : ICommand;
 }
