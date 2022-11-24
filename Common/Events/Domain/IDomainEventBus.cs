642e5999b084519e9a312325b651c3c7edfcfb0d
Benjamin <beela93@hotmail.com>
2022-11-24 10:33:48 +0100

    commented out a method that, currently, cannot work

--- Common/Events/Domain/IDomainEventBus.cs
+++ Common/Events/Domain/IDomainEventBus.cs
@@ -3,5 +3,5 @@ public interface IDomainEventBus
 {
     public void RegisterHandler<T>(Action<T> handler) where T : IDomainEvent;
     public void Publish<T>(T @event) where T : IDomainEvent;
-    public void UnregisterHandler<T>(Action<T> handler) where T : IDomainEvent;
+    //public void UnregisterHandler<T>(Action<T> handler) where T : IDomainEvent;
 }
