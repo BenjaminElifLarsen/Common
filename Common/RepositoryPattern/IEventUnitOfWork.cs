5349885e6b66c4481a426eb56b692e42c27d6354
Benjamin <beela93@hotmail.com>
2022-12-02 07:46:48 +0100

    removed base context contract as it was unsure if it would be used in other projects, added unit of work for events

---
+++ Common/RepositoryPattern/IEventUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using Common.Events.Domain;
+
+namespace Common.RepositoryPattern;
+public interface IEventUnitOfWork
+{
+    public void AddOrphanEvnet(IDomainEvent @event);
+    public void ProcessEvents();
+}
