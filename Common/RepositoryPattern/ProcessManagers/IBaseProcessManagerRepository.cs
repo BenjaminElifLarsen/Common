86a83e9b3dfcf05d8552362e68723ddb2a907212
Benjamin <beela93@hotmail.com>
2023-01-03 14:48:24 +0100

    made a method async

--- Common/RepositoryPattern/ProcessManagers/IBaseProcessManagerRepository.cs
+++ Common/RepositoryPattern/ProcessManagers/IBaseProcessManagerRepository.cs
@@ -3,7 +3,7 @@
 namespace Common.RepositoryPattern.ProcessManagers;
 public interface IBaseProcessManagerRepository<TProcessManager> where TProcessManager : BaseProcessManager
 {
-    public TProcessManager Load(Guid correlationId);
+    public Task<TProcessManager> LoadAsync(Guid correlationId);
     public void Save(TProcessManager processManager);
     public void Delete(Guid correlationId); //should it be possible to delete a pm? If yes, when?
 }
