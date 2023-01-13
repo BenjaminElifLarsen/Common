fa4cb632d98a697a4d5f99571d5815e3c98f3bb0
Benjamin <beela93@hotmail.com>
2023-01-13 10:50:48 +0100

    added contracts for handling Event

---
+++ Common/Events/Conversion/IGetEvent.cs
@@ -0,0 +1,8 @@
+﻿using Common.Events.Domain;
+using Common.Events.Store.Event;
+
+namespace Common.Events.Conversion;
+public interface IGetEvent<TEvent> where TEvent : DomainEvent
+{
+    public static abstract IEnumerable<DataPoint> Get(TEvent @event);
+}
---
+++ Common/Events/Conversion/ISetEvent.cs
@@ -0,0 +1,8 @@
+﻿using Common.Events.Domain;
+using Common.Events.Store.Event;
+
+namespace Common.Events.Conversion;
+public interface ISetEvent
+{
+    public static abstract DomainEvent Set(Event @event);
+}
