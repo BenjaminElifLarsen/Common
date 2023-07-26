1cc1c35de52e4ec7ec1b2096ed16b9985870a670
Nia <beela93@hotmail.com>
2023-07-26 17:31:45 +0200

    storage file, considering something

---
+++ Common/Storage/ICommandAndEventStorage.cs
@@ -0,0 +1,15 @@
+﻿using Common.CQRS.Commands;
+using Common.Events.Base;
+using Common.Events.Store.Event;
+
+namespace Common.Storage;
+
+public interface ICommandAndEventStorage
+{
+
+    public void Add(ICommand command);
+
+    public void Add(IBaseEvent @event);
+
+    public bool Process();
+}
