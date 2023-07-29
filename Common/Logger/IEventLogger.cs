9a4ca21a19393c686a3871906d415b6a7e86ab51
Nia <beela93@hotmail.com>
2023-07-29 21:17:56 +0200

    base logger

---
+++ Common/Logger/IEventLogger.cs
@@ -0,0 +1,7 @@
+﻿using Common.Events.Base;
+
+namespace Common.Logger;
+public interface IEventLogger
+{
+    public void Handle(IBaseEvent @event);
+}
---
+++ Common/Logger/TestingEventLogger.cs
@@ -0,0 +1,11 @@
+﻿using Common.Events.Base;
+using System.Diagnostics;
+
+namespace Common.Logger;
+public class TestingEventLogger : IEventLogger
+{
+    public void Handle(IBaseEvent @event)
+    {
+        Debug.WriteLine($"{@event.CorrelationId} : {@event.CausationId} : {@event.EventId} : {@event.GetType()}");
+    }
+}
